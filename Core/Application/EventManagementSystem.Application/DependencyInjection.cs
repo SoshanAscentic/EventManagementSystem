8879fee2a7369c977987581a8eea28e6400598bf
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-07-17 09:09:47 +0530

    created the dtos and dependency injection of the application layer

---
+++ Core/Application/EventManagementSystem.Application/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="DependencyInjection.cs" company="Ascentic">
+// Copyright (c) Ascentic. All rights reserved.
+// </copyright>
+
+namespace EventManagementSystem.Application
+{
+    using System.Reflection;
+    using EventManagementSystem.Application.Common.Behaviors;
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Register MediatR
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+            // Register AutoMapper
+            services.AddAutoMapper(cfg => cfg.AddMaps(assembly));
+
+            // Register FluentValidation
+            services.AddValidatorsFromAssembly(assembly);
+
+            // Register MediatR behaviors
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+
+            return services;
+        }
+    }
+}
